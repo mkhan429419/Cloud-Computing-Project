911eb8c9db2ca6fecc1c31a8e1e2fb10b5e8b7e8
Maheen Akhtar Khan <116183925+mkhan429419@users.noreply.github.com>
2024-12-17 17:23:18 +0500

    update

--- monitor-logging/models/usage.js
+++ monitor-logging/models/usage.js
@@ -1,22 +1,11 @@
-// const mongoose = require('mongoose');
-
-// const usageSchema = new mongoose.Schema({
-//     userId: { type: String, required: true }, 
-//     date: { type: Date, default: new Date().setHours(0, 0, 0, 0) },
-//     usedBandwidth: { type: Number, default: 0 },
-//     log: { type: String, required: true }, 
-// });
-
-// module.exports = mongoose.model('Usage', usageSchema);
-
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const usageSchema = new mongoose.Schema({
-    firebaseUserId: { type: String, required: true },
-    date: { type: Date, default: new Date() },
-    //date: { type: Date, default: Date.now },
-    timestamp: { type: Date, default: Date.now },
-    dailyBandwidthUsed: { type: Number, default: 0 },
+  firebaseUserId: { type: String, required: true },
+  date: { type: Date, default: new Date() },
+  //date: { type: Date, default: Date.now },
+  timestamp: { type: Date, default: Date.now },
+  dailyBandwidthUsed: { type: Number, default: 0 },
 });
 
-module.exports = mongoose.model('Usage', usageSchema);
+module.exports = mongoose.model("Usage", usageSchema);
